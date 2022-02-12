dfa1b5eb1007d194bca291fe849fb11fee49c942
SafiqJasnai <87291280+SafiqJasnai@users.noreply.github.com>
2022-02-12 20:47:16 +0800

    Upload header.php

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">    
+
+    <link rel="stylesheet" href="css/iss.css" type="text/css">
+    
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+
+    <script src="https://cdn-webgl.wrld3d.com/wrldjs/dist/latest/wrld.js"></script>
+
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+            integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+            crossorigin="">
+    </script>
+
+</head>
