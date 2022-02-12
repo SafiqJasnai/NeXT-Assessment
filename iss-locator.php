36d8c3b1db63eeed6939aeb681278f6c065d9c15
SafiqJasnai <87291280+SafiqJasnai@users.noreply.github.com>
2022-02-12 20:50:02 +0800

    Upload iss-locator.php
    Choose date and time to find ISS location and timestamp

---
+++ iss-locator.php
@@ -0,0 +1,12 @@
+<section class="isslocator">
+
+    <div id="infoi" class="container">
+    	<form action="iss-timestamp.php" method="post">
+    		<h2>ISS Locator</h2>
+	    	<input type="datetime-local" name="issDatetime">
+	    	<p id="help-box">Select Date & Time</p>
+	    	<button  class="btn" type="submit">Check</button>
+    	</form>
+    </div>
+
+</section>
