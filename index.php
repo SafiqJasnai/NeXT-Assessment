26361d57cbebbf8d6465a7e5348b577eb0a2aa22
SafiqJasnai <87291280+SafiqJasnai@users.noreply.github.com>
2022-02-12 20:48:17 +0800

    Upload index.php
    ISS Locator Homepage

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include('header.php'); ?>
+
+<title>ISS Locator</title>
+
+<body background="https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/bbbf2a17-987c-4fe6-b190-ac8fe0e235e9">
+
+	<div class="wrapper">
+
+		<h1><a href="index.php">International Space Station</a></h1>
+		<br>
+
+		<?php include ('iss-locator.php') ?>
+
+		<?php include ('iss-map.php'); ?>
+
+		<?php include ('iss-inside.php'); ?>
+
+		<?php include ('iss-twitter.php'); ?>
+
+	</div>
+
+
+
+</body>
+
+
+
