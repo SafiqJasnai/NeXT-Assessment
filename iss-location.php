c92e4aeaf9691030922b8b473c14088df10b2e7d
SafiqJasnai <87291280+SafiqJasnai@users.noreply.github.com>
2022-02-18 11:20:38 +0800

    Update iss-location.php

--- iss-location.php
+++ iss-location.php
@@ -2,23 +2,43 @@
 
 <title>ISS Locator</title>
 <?php 
-  if(empty($_POST['issDatetime'])){
-    header("Location: index.php");
-    die();
-  }
-  
-  $getTime = $_POST['issDatetime'];
-  $genTimestamp = strtotime($getTime);
+    if(empty($_POST['issDatetime'])){
+        header("Location: index.php");
+        die();
+    }
+    
+    /* Convert datetime to timestamp */
+    $getTime = $_POST['issDatetime'];
+    $genTimestamp = strtotime($getTime);
 
-  $time = [];
-  $time[6] = $genTimestamp ;
+    $time = [];
+    $time[6] = $genTimestamp ;
 
-  for ($j = 5; $j >= 0 ; $j--) {
-    $time[$j] = $time[$j+1]-600;
-  }
-  for ($t = 7; $t <= 12; $t++) {
-    $time[$t] = $time[$t-1]+600;
-  } 
+    for ($j = 5; $j >= 0 ; $j--) {
+        $time[$j] = $time[$j+1]-600;
+    }
+    for ($t = 7; $t <= 12; $t++) {
+        $time[$t] = $time[$t-1]+600;
+    } 
+
+    /* ISS Location using https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps={timestamp} */
+    $strtourl = "https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps=$time[0],$time[1],$time[2],$time[3],$time[4],$time[5],$time[6],$time[7],$time[8],$time[9],$time[10],$time[11],$time[12]&units=miles";
+    $url = $strtourl;
+
+    $cURL = curl_init();
+                
+    curl_setopt($cURL, CURLOPT_URL, $url);
+    curl_setopt($cURL, CURLOPT_HTTPGET, false);
+    curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
+                
+    curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Accept: application/json'
+    ));
+                
+    $result = curl_exec($cURL);
+    curl_close($cURL);
+    $arrays =  json_decode($result);
 ?>
 
 <style>
@@ -46,72 +66,6 @@ table tr td:first-child::before
         <br>
         <hr>
         <?php include ('iss-locator.php'); ?>
-        <br>
-        <h2 style="text-align:center; background-color:#212121;color:#ffffff">ISS Timestamp (<?php echo $_POST['issDatetime'] ?>)</h2>
-        <div class="container" style="margin-bottom: 10em;">
-        
-            <br>
-            <?php
-
-                $strtourl = "https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps=$time[0],$time[1],$time[2],$time[3],$time[4],$time[5],$time[6],$time[7],$time[8],$time[9],$time[10],$time[11],$time[12]&units=miles";
-
-                $url = $strtourl;
-
-                $cURL = curl_init();
-                
-                curl_setopt($cURL, CURLOPT_URL, $url);
-                curl_setopt($cURL, CURLOPT_HTTPGET, false);
-                curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
-                
-                curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
-                    'Content-Type: application/json',
-                    'Accept: application/json'
-                ));
-                
-                $result = curl_exec($cURL);
-                curl_close($cURL);
-                $arrays =  json_decode($result);
-
-            ?>
-
-            <table class="tableCoor">
-                <thead>
-                    <tr class="tableCoor-head">
-                        <th style="color:#212121">No.</th> 
-                        <th style="color:#212121">Timestamp</th> 
-                        <th style="color:#212121">Id</th> 
-                        <th style="color:#212121">Name</th> 
-                        <th style="color:#212121">Longitude</th>
-                        <th style="color:#212121">Latitude</th>
-                    </tr>
-                </thead>
-
-                <?php 
-                foreach ($arrays as $value){  
-                ?>
-                <tr>
-                    <td></td>
-                    <td>&nbsp&nbsp<?php echo $value -> timestamp ?> </td>
-                    <td><?php echo $value -> id ?> </td> 
-                    <td><?php echo $value -> name ?> </td> 
-                    <td><?php echo $value -> latitude ?> </td>
-                    <td><?php echo $value -> longitude ?> </td>
-                </tr>
-
-                <?php  } ?>            
-
-            </table>
-            <br>
-            <p id="legend">
-                Timestamp 7 : Selected Time & Date <br>
-                Timestamp 1-6 : 10 minutes backward <br>
-                Timestamp 8-13 : 10 minutes forward 
-            </p>
-            <br>
-
-        </div>
-        <br>
-        <br>
         <br>
         <h2 style="text-align:center; background-color:#212121;color:#ffffff">ISS Location (<?php echo $_POST['issDatetime'] ?>)</h2>
         <br>
@@ -157,5 +111,45 @@ table tr td:first-child::before
                 </script>
             </div> 
         </section>
+        <h2 style="text-align:center; background-color:#212121;color:#ffffff">ISS Timestamp (<?php echo $_POST['issDatetime'] ?>)</h2>
+        <div class="container" style="margin-bottom: 10em;">
+        
+            <br>
+            <table class="tableCoor">
+                <thead>
+                    <tr class="tableCoor-head">
+                        <th style="color:#212121">No.</th> 
+                        <th style="color:#212121">Timestamp</th> 
+                        <th style="color:#212121">Id</th> 
+                        <th style="color:#212121">Name</th> 
+                        <th style="color:#212121">Latitude</th>
+                        <th style="color:#212121">Longtitude</th>
+                    </tr>
+                </thead>
+
+                <?php 
+                foreach ($arrays as $value){  
+                ?>
+                <tr>
+                    <td></td>
+                    <td>&nbsp&nbsp<?php echo $value -> timestamp ?> </td>
+                    <td><?php echo $value -> id ?> </td> 
+                    <td><?php echo $value -> name ?> </td> 
+                    <td><?php echo $value -> latitude ?> </td>
+                    <td><?php echo $value -> longitude ?> </td>
+                </tr>
+
+                <?php  } ?>            
+
+            </table>
+            <br>
+            <p id="legend">
+                Timestamp 7 : Selected Time & Date <br>
+                Timestamp 1-6 : 10 minutes backward <br>
+                Timestamp 8-13 : 10 minutes forward 
+            </p>
+            <br>
+
+        </div>
     </div>
 </body>
