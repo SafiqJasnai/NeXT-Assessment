c7a70d91b69d46c044379eef6b90035324a0fcd9
SafiqJasnai <87291280+SafiqJasnai@users.noreply.github.com>
2022-02-18 11:07:42 +0800

    Update iss-twitter.php

--- iss-twitter.php
+++ iss-twitter.php
@@ -1,4 +1,4 @@
-<h2 style="text-align:center; background-color:#212121;color:#ffffff">Tweets</h2>
+<h2 style="text-align:center; background-color:#212121;color:#ffffff">@Space_Station Tweets</h2>
 <br>
 <div id="container" style="text-align:center">
 	<a class="twitter-timeline" data-width="500" data-height="500" data-theme="dark" href="https://twitter.com/Space_Station?ref_src=twsrc%5Etfw">Tweets by Space_Station</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
